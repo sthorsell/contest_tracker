b4b03c75c484cf6e613abbcfae5a3e4031749bd6
Steve Thorsell <stevothorsell@gmail.com>
2020-05-19 08:57:37 -0500

    Convert JS tabs to spaces

--- assets/js/Contest.js
+++ assets/js/Contest.js
@@ -7,73 +7,73 @@ import { Table, TableBody, TableCell, TableHead, TablePagination, TableRow } fro
 
 
 function Summary(props) {
-	const { open, onClose, contest } = props
-	const [payoutPage, setPayoutPage] = React.useState(0);
-	const [filteredPayouts, setFilteredPayouts] = React.useState([])
+  const { open, onClose, contest } = props
+  const [payoutPage, setPayoutPage] = React.useState(0);
+  const [filteredPayouts, setFilteredPayouts] = React.useState([])
 
-	const handleClose = () => {
+  const handleClose = () => {
     onClose();
   };
 
   useEffect(() => {
-  	const start = payoutPage * 5
-  	setFilteredPayouts(() => contest.payouts.slice(start, start + 5))
+    const start = payoutPage * 5
+    setFilteredPayouts(() => contest.payouts.slice(start, start + 5))
   }, [payoutPage]);
 
-	return (
+  return (
     <Dialog onClose={handleClose} aria-labelledby='contest-summary' open={open} PaperProps={{style: {margin: '8px'}}}>
       <DialogTitle id='simple-dialog-title' disableTypography={true}><Typography variant="subtitle2">{contest.name}</Typography></DialogTitle>
       <DialogContent dividers>
-      	<Grid container justify="center">
-      		<Grid item xs={6}>
-      			<Typography variant="body2">Entry Fee: ${contest.entryFee}</Typography>
-      		</Grid>
-      		<Grid item xs={6}>
-      			<Typography variant="body2">Total Entries: {Number(contest.entryCount).toLocaleString()}</Typography>
-      		</Grid>
-      		<Grid item xs={12}>
-      			<Table aria-label="payouts">
-			        <TableHead>
-			          <TableRow>
-			            <TableCell>Position</TableCell>
-			            <TableCell align="right">Prize($)</TableCell>
-			          </TableRow>
-			        </TableHead>
-			        <TableBody>
-			          {filteredPayouts.map((row, i) => (
-			            <TableRow key={i}>
-			              <TableCell component="th" scope="row">
-			              	{
-			              		row.min === row.max ? 
-			              		<React.Fragment>{row.min}</React.Fragment> :
-			              		<React.Fragment>{row.min} - {row.max}</React.Fragment>
-			              	}
-			              </TableCell>
-			              <TableCell align="right">{Number(row.amount).toLocaleString()}</TableCell>
-			            </TableRow>
-			          ))}
-			        </TableBody>
-			      </Table>
-			      <TablePagination
-		          component="div"
-		          count={contest.payouts.length}
-		          rowsPerPage={5}
-		          rowsPerPageOptions={[]}
-		          page={payoutPage}
-		          onChangePage={(event, page) => setPayoutPage(page)}
-		        />
-      		</Grid>
-      	</Grid>
+        <Grid container justify="center">
+          <Grid item xs={6}>
+            <Typography variant="body2">Entry Fee: ${contest.entryFee}</Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="body2">Total Entries: {Number(contest.entryCount).toLocaleString()}</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Table aria-label="payouts">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Position</TableCell>
+                  <TableCell align="right">Prize($)</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {filteredPayouts.map((row, i) => (
+                  <TableRow key={i}>
+                    <TableCell component="th" scope="row">
+                      {
+                        row.min === row.max ? 
+                        <React.Fragment>{row.min}</React.Fragment> :
+                        <React.Fragment>{row.min} - {row.max}</React.Fragment>
+                      }
+                    </TableCell>
+                    <TableCell align="right">{Number(row.amount).toLocaleString()}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+            <TablePagination
+              component="div"
+              count={contest.payouts.length}
+              rowsPerPage={5}
+              rowsPerPageOptions={[]}
+              page={payoutPage}
+              onChangePage={(event, page) => setPayoutPage(page)}
+            />
+          </Grid>
+        </Grid>
       </DialogContent>
     </Dialog>
   );
 }
 
 export default function Contest() {
-	const [contest, setContest] = React.useState(null);
-	const [open, setOpen] = React.useState(false);
+  const [contest, setContest] = React.useState(null);
+  const [open, setOpen] = React.useState(false);
 
-	const handleClickOpen = () => {
+  const handleClickOpen = () => {
     setOpen(true);
   };
 
@@ -81,35 +81,35 @@ export default function Contest() {
     setOpen(false);
   };
 
-	useEffect(() => {
-		fetch('api/summary')
-			.then(response => response.json())
-			.then(data => setContest(data));
-	}, []);
+  useEffect(() => {
+    fetch('api/summary')
+      .then(response => response.json())
+      .then(data => setContest(data));
+  }, []);
 
-	const loading = <Typography variant="h6">Loading...</Typography>
-	
+  const loading = <Typography variant="h6">Loading...</Typography>
+  
 
   return(
-  	<Grid item xs={12}>
-  		{
-  			contest ? 
-				<Grid container alignItems="center">
-					<Grid item xs={false} md={4} />
-	  			<Grid item xs={10} md={4}>
-						<Typography variant="subtitle2" noWrap>
-						  	{contest.name}		  	      	
-						</Typography>
-					</Grid>
-					<Grid item xs={2} md={4}>
-						<IconButton onClick={handleClickOpen}>
-  		        <Info style={{ fontSize: 20 }} />
-  		      </IconButton>
-					</Grid>
-					<Summary open={open} onClose={handleClose} contest={contest} />
-				</Grid> :
-  			'Loading...'
-  		}  		
-  	</Grid>
+    <Grid item xs={12}>
+      {
+        contest ? 
+        <Grid container alignItems="center">
+          <Grid item xs={false} md={4} />
+          <Grid item xs={10} md={4}>
+            <Typography variant="subtitle2" noWrap>
+                {contest.name}                
+            </Typography>
+          </Grid>
+          <Grid item xs={2} md={4}>
+            <IconButton onClick={handleClickOpen}>
+              <Info style={{ fontSize: 20 }} />
+            </IconButton>
+          </Grid>
+          <Summary open={open} onClose={handleClose} contest={contest} />
+        </Grid> :
+        'Loading...'
+      }     
+    </Grid>
   );
 }
--- assets/js/Root.js
+++ assets/js/Root.js
@@ -11,106 +11,106 @@ const useStyles = makeStyles(() => ({
     paddingLeft: '16px',
     paddingRight: '16px'
   },
-  	marked: {
-  		marginBottom: '0px'
-  	}
+  marked: {
+    marginBottom: '0px'
+  }
 }));
 
 export default function Root() {
-	const [quarter, setQuarter] = React.useState(1);
-	const [selectedPlay, setSelectedPlay] = React.useState(0);
-	const [plays, setPlays] = React.useState([]);
-	const [filteredPlays, setFilteredPlays] = React.useState([]);
-	const [maxPlayIndex, setMaxPlayIndex] = React.useState(0);
-	const [entries, setEntries] = React.useState([]);
-	const [entryPage, setEntryPage] = React.useState(0);
-	const [sliderMarks, setSliderMarks] = React.useState([]);
-	const classes = useStyles();
+  const [quarter, setQuarter] = React.useState(1);
+  const [selectedPlay, setSelectedPlay] = React.useState(0);
+  const [plays, setPlays] = React.useState([]);
+  const [filteredPlays, setFilteredPlays] = React.useState([]);
+  const [maxPlayIndex, setMaxPlayIndex] = React.useState(0);
+  const [entries, setEntries] = React.useState([]);
+  const [entryPage, setEntryPage] = React.useState(0);
+  const [sliderMarks, setSliderMarks] = React.useState([]);
+  const classes = useStyles();
 
-	useEffect(() => {
-		fetch('api/plays')
-			.then(response => response.json())
-			.then(data => {
-				setPlays(data);
-				const filtered = data.filter(p => p.quarter === quarter);
-				setFilteredPlays(filtered);
-			});
-	}, []);
+  useEffect(() => {
+    fetch('api/plays')
+      .then(response => response.json())
+      .then(data => {
+        setPlays(data);
+        const filtered = data.filter(p => p.quarter === quarter);
+        setFilteredPlays(filtered);
+      });
+  }, []);
 
-	useEffect(() => {
-		setFilteredPlays(plays.filter(p => p.quarter === quarter))
-	}, [quarter]);
+  useEffect(() => {
+    setFilteredPlays(plays.filter(p => p.quarter === quarter))
+  }, [quarter]);
 
   const handleChange = (event) => {
     setQuarter(event.target.value);
   };
 
   useEffect(() => {
-  	setEntryPage(0)
+    setEntryPage(0)
   }, [selectedPlay]);
 
   useEffect(() => {
-  	setSelectedPlay(0);
+    setSelectedPlay(0);
   }, [quarter]);
 
   useEffect(() => {
-  	setMaxPlayIndex(filteredPlays.length - 1);
+    setMaxPlayIndex(filteredPlays.length - 1);
 
-  	const marks = filteredPlays.reduce((res, play, i) => {
-  		if(['FG', 'TD'].includes(play.note)){
-  			res.push({value: i, label: play.note});
-  		}
-  		return res;
-  	}, []);
-  	setSliderMarks(marks);
+    const marks = filteredPlays.reduce((res, play, i) => {
+      if(['FG', 'TD'].includes(play.note)){
+        res.push({value: i, label: play.note});
+      }
+      return res;
+    }, []);
+    setSliderMarks(marks);
   }, [filteredPlays]);
 
   useEffect(() => {
-  	const play = filteredPlays[selectedPlay]
-  	if(play){
-  		fetch(`api/entries/${play.id}/${entryPage}`)
-  			.then(response => response.json())
-  			.then(data => {
-  				setEntries(data)
-  			})
-  	}
+    const play = filteredPlays[selectedPlay]
+    if(play){
+      fetch(`api/entries/${play.id}/${entryPage}`)
+        .then(response => response.json())
+        .then(data => {
+          setEntries(data)
+        })
+    }
   }, [entryPage, selectedPlay, filteredPlays])
 
   return(
-  	<div>
-  		<CssBaseline />
+    <div>
+      <CssBaseline />
       <AppBar position="static">
-	      <Toolbar>
-	        <Typography variant="h6">
-	          	Contest Tracker
-	        </Typography>
-	      </Toolbar>
-	    </AppBar>
-	    <Grid container className={classes.root} direction="row" justify="center">
-	    	<Contest />
-	    	<Grid item xs={4} align="left">
-		      <Select
-		        labelId="quarter-label"
-		        id="quarter"
-		        value={quarter}
-		        onChange={handleChange}
-		      >
-		    		{[1,2,3,4].map(i => (
-		    			<MenuItem value={i} key={i}>Quarter {i}</MenuItem>
-		    		))}
-		      </Select>
-	      </Grid>
-	      <Grid item xs={4}>
-		      <Typography variant="h6" align="center" gutterBottom>
-	          Chiefs: {filteredPlays[selectedPlay] ? filteredPlays[selectedPlay].score.home : 0}
-	        </Typography>
-	      </Grid>
-	      <Grid item xs={4}>
-		      <Typography variant="h6" align="right" gutterBottom>
-	          49ers: {filteredPlays[selectedPlay] ? filteredPlays[selectedPlay].score.away : 0}
-	        </Typography>
-	      </Grid>
-	    </Grid>
+        <Toolbar>
+          <Typography variant="h6">
+              Contest Tracker
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Grid container className={classes.root} direction="row" justify="center">
+        <Contest />
+        <Grid item xs={4} align="left">
+          <Select
+            labelId="quarter-label"
+            id="quarter"
+            value={quarter}
+            onChange={handleChange}
+          >
+            {[1,2,3,4].map(i => (
+              <MenuItem value={i} key={i}>Quarter {i}</MenuItem>
+            ))}
+          </Select>
+        </Grid>
+        <Grid item xs={4}>
+          <Typography variant="h6" align="center" gutterBottom>
+            Chiefs: {filteredPlays[selectedPlay] ? filteredPlays[selectedPlay].score.home : 0}
+          </Typography>
+        </Grid>
+        <Grid item xs={4}>
+          <Typography variant="h6" align="right" gutterBottom>
+            49ers: {filteredPlays[selectedPlay] ? filteredPlays[selectedPlay].score.away : 0}
+          </Typography>
+        </Grid>
+      </Grid>
 
       <Grid item xs={12} className={classes.root} >
         <Typography variant="body2" gutterBottom style={{minHeight: "78px"}}>
@@ -118,30 +118,30 @@ export default function Root() {
         </Typography>
       </Grid>
 
-    	<Grid container direction="row" justify="center" alignItems="center" style={{paddingBottom: '8px'}}>
-	    	<Grid item xs={2} align="center">
-	      	<IconButton aria-label="previous play" disabled={selectedPlay === 0} onClick={() => setSelectedPlay(play => play - 1)} style={{paddingTop: '8px'}}>
-		        <ArrowBackIos />
-		      </IconButton>
-	      </Grid>
-	      <Grid item xs={8}>
-	        <Slider
-	          value={selectedPlay}
-	          aria-labelledby="discrete-slider-small-steps"
-	          classes={{marked: classes.marked}}
-	          step={1}
-	          marks={sliderMarks}
-	          min={0}
-	          max={maxPlayIndex}
-	          onChange={(_, i) =>  setSelectedPlay(i) }
-	          valueLabelDisplay="off"
-	        />
-	      </Grid>
-	    	<Grid item xs={2} align="center">
-	      	<IconButton aria-label="previous play" disabled={maxPlayIndex === selectedPlay} onClick={() => setSelectedPlay(play => play + 1)} style={{paddingTop: '8px'}}>
-		        <ArrowForwardIos />
-		      </IconButton>
-	      </Grid>
+      <Grid container direction="row" justify="center" alignItems="center" style={{paddingBottom: '8px'}}>
+        <Grid item xs={2} align="center">
+          <IconButton aria-label="previous play" disabled={selectedPlay === 0} onClick={() => setSelectedPlay(play => play - 1)} style={{paddingTop: '8px'}}>
+            <ArrowBackIos />
+          </IconButton>
+        </Grid>
+        <Grid item xs={8}>
+          <Slider
+            value={selectedPlay}
+            aria-labelledby="discrete-slider-small-steps"
+            classes={{marked: classes.marked}}
+            step={1}
+            marks={sliderMarks}
+            min={0}
+            max={maxPlayIndex}
+            onChange={(_, i) =>  setSelectedPlay(i) }
+            valueLabelDisplay="off"
+          />
+        </Grid>
+        <Grid item xs={2} align="center">
+          <IconButton aria-label="previous play" disabled={maxPlayIndex === selectedPlay} onClick={() => setSelectedPlay(play => play + 1)} style={{paddingTop: '8px'}}>
+            <ArrowForwardIos />
+          </IconButton>
+        </Grid>
       </Grid>
 
       <EntriesTable entries={entries} entryPage={entryPage} onChangePage={setEntryPage} />
